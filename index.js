60e518c733c753814c1ccbe6a1449f0e29d846a6
Fati-810 <49760018+Fati-810@users.noreply.github.com>
2026-02-27 13:06:02 +0400

    Switch to using pg Pool for database connections
    Refactor database connection to use connection pool.

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ dotenv.config();
 
 const app = express();
 const port = 3000;
+const { Pool } = require("pg");
 
-const db = new pg.Client({
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
+});
+/*const db = new pg.Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-});
+});*/
 
 db.connect();
 
