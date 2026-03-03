aefb7e39103787d9c97e2416c94a92bef67ac28e
Fati-810 <49760018+Fati-810@users.noreply.github.com>
2026-03-03 21:53:54 +0400

    Refactor app.listen to use dynamic PORT
    Updated server to use environment variable for port

--- index.js
+++ index.js
@@ -266,9 +266,13 @@ app.get("/suggestions", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => console.log(`Running on ${PORT}`));
+
+/*app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
-});
+});*/
 
     
     
